2fea56f3c2fe32fc1c295ae163fb094df92f87fa
softdevomar <omorfarukqqq@gmail.com>
2024-07-11 23:30:40 -0700

    user update method create done

--- controllers/user/user_controller.js
+++ controllers/user/user_controller.js
@@ -1,14 +1,42 @@
-const User = require('../../models/user_model')
+const User = require("../../models/user_model");
+const bcrypt = require("bcrypt");
+//get all user
 exports.getAllUser = async (req, res, next) => {
-	try {
-		const user = await User.find();
-		  res.status(200).json({
-			  massege: "data get succesfully",
-			  user,
-      });
+  try {
+    const user = await User.find();
+    res.status(200).json({
+      massage: "data get succesfully",
+      user,
+    });
   } catch (error) {
     res.status(401).json({
       massege: "data not found",
+      error: error.message,
+    });
+  }
+};
+
+// update profile user
+exports.updateUser = async (req, res, next) => {
+  const userId = req.params.userId;
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(400).json({
+        massage: "wrong user!",
+      });
+    }
+   // req.body.password = await bcrypt.hash(req.body.password, 11);
+    const updateUser = await User.findByIdAndUpdate(userId, req.body, {
+      new: true,
+    });
+    res.status(200).json({
+      massage: "user update successfully",
+      updateUser,
+    });
+  } catch (error) {
+    res.status(401).json({
+      massage: "user not update",
       error: error.message,
     });
   }
--- routes/user/user_route.js
+++ routes/user/user_route.js
@@ -1,6 +1,7 @@
-const { getAllUser } = require("../../controllers/user/user_controller");
+const { getAllUser, updateUser } = require("../../controllers/user/user_controller");
 const { authMiddleware } = require("../../middlewares/auth_middleware");
 
 const userRoute = require("express").Router();
-userRoute.get("/",authMiddleware, getAllUser);
+userRoute.get("/get-all-user",authMiddleware, getAllUser);
+userRoute.put("/:userId",authMiddleware,updateUser);
 module.exports = userRoute;
